919b451c37e9a37cdd24106578f939f2d3ed1411
Ralf Mayet <trilly@gmail.com>
2021-05-05 16:04:24 +0200

    commiting original code from ODRI

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # emma-simulation
 Code related to simulation of the Emma Bot
+
+
+## instructions
+
+1. Install Pinochio using `conda install -c conda-forge pinocchio`
+2. install `robot_properties_bolt`, `robot_properties_solo` and `bullet_utils` repos
+
+
+## running
+
+...
---
+++ simulate_bolt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""demo_simulate_bolt
+
+Original source: https://github.com/open-dynamic-robot-initiative/robot_properties_bolt/blob/master/demos/demo_simulate_bolt.py
+
+Basic loading and visualization for the Bolt robot using gepetto viewer.
+
+@file display_bolt.py
+@copyright Copyright (c) 2020,
+           New York University and Max Planck Gesellschaft,
+           License BSD-3-Clause
+@example display_bolt.py
+- 1. Building the workspace by executing `catkin build` in the workspace.
+- 2. "source ./devel/setup.bash" is called from the root of the catkin
+     workspace.
+- 3. Run the demo by either:
+    - 3.1. `python3 display_bolt.py`
+    - 3.2. `cd /path/to/robot_properties_bolt/` ; `./demos/display_bolt.py`
+"""
+
+import time
+import numpy as np
+from bullet_utils.env import BulletEnvWithGround
+from robot_properties_bolt.bolt_wrapper import BoltRobot, BoltConfig
+import pybullet as p
+
+
+if __name__ == "__main__":
+    # Create a robot instance. This initializes the simulator as well.
+    env = BulletEnvWithGround(p.GUI) # DIRECT for non-gui
+    robot = env.add_robot(BoltRobot)
+    tau = np.zeros(robot.nb_dof)
+
+    # Reset the robot to some initial state.
+    q0 = BoltConfig.initial_configuration
+    dq0 = BoltConfig.initial_velocity
+    robot.reset_state(q0, dq0)
+
+    # Run the simulator for 100 steps
+    for i in range(10000):
+        # TODO: Implement a controller here.
+        robot.send_joint_command(tau)
+
+        # Step the simulator.
+        env.step(sleep=True) # You can use sleep here if you want to slow down the replay
+
+    # Read the final state and forces after the stepping.
+    q, dq = robot.get_state()
+    active_eff, forces = robot.get_force()
+    print('q', q)
+    print('dq', dq)
+    print('active eff', active_eff)
+    print('forces', forces)
+ 
